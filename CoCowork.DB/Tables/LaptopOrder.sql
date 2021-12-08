a921e9f2a54b206ef317130d56e417deebba1e68
BarBoss228 <nastia.polpoudenko@gmail.com>
2021-12-08 18:47:06 +0300

    fixed FK for LaptopOrder

--- CoCowork.DB/Tables/LaptopOrder.sql
+++ CoCowork.DB/Tables/LaptopOrder.sql
@@ -2,9 +2,9 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	[LaptopId] int NOT NULL, 
-    FOREIGN KEY (LaptopId) REFERENCES Laptop(Id),
 	[OrderId] int NOT NULL,
 	[StartDate] datetime NOT NULL,
 	[EndDate] datetime NOT NULL,
-	[SubtotalPrice] int NOT NULL
+	[SubtotalPrice] int NOT NULL,
+	CONSTRAINT [FK_LaptopId_to_Laptop] FOREIGN KEY ([LaptopId]) REFERENCES [Laptop]([Id])
 )
