4a8b0915d74edab6b19f2b1bc1137fe08dfee4c6
Sofa <sofarmakovski@gmail.com>
2021-12-08 22:19:44 +0300

    created Product and ProductOrder tables

---
+++ CoCowork.DB/Tables/Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Name] varchar(30) NOT NULL,
+	[Amount] int NOT NULL,
+	[PriceForOne] int NOT NULL,
+	[Description] nvarchar(200) NOT NULL
+)
---
+++ CoCowork.DB/Tables/ProductOrder.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProductOrder]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+	[ProductId] INT NOT NULL,
+	[OrderId] INT NOT NULL,
+	[Amount] INT NOT NULL,
+	[SubtotaPrice] INT NOT NULL,
+	CONSTRAINT [FK_ProductId_To_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id])
+)
