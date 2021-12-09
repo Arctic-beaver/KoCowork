0de1fba55b446df9ed571c40a370b16952ab34dd
Arctic Beaver <ArcticBeaver@yandex.ru>
2021-12-09 12:55:42 +0300

    CRUD for Place is ready
    Without join

---
+++ CoCowork.DB/StoredProcedures/Place.CRUD/Place_Insert.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROC dbo.Place_Insert
+	@MiniOfficeId int,
+	@PricePerDay int,
+	@PriceFixedPerDay int
+AS
+BEGIN
+	insert into dbo.Place
+		(
+			MiniOfficeId, 
+			PricePerDay, 
+			PriceFixedPerDay
+		)
+	values 
+		(
+			@MiniOfficeId,
+			@PricePerDay,
+			@PriceFixedPerDay
+		)
+END
---
+++ CoCowork.DB/StoredProcedures/Place.CRUD/Place_Update.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROC dbo.Place_Update
+	@Id int,
+	@MiniOfficeId int,
+	@PricePerDay int,
+	@PriceFixedPerDay int
+AS
+BEGIN
+	update dbo.Place
+	set MiniOfficeId = @MiniOfficeId,
+	    PricePerDay = @PricePerDay,
+		PriceFixedPerDay = @PriceFixedPerDay
+    where Id = @Id
+END
