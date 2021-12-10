726e982cc94efcff72f94ba9cf210ed093f6184c
ll <jokzik@gmail.com>
2021-12-10 15:48:46 +0300

    Fix bugs

--- CoCowork.DB/Tables/Order.sql
+++ CoCowork.DB/Tables/Order.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ClientId] INT NOT NULL, 
-    [TotalPrice] DECIMAL NULL, 
+    [TotalPrice] DECIMAL(10, 2) NOT NULL, 
     [IsPaid] BIT NOT NULL, 
     [IsCanceled] BIT NOT NULL, 
     CONSTRAINT [FK_Order_ToTable] FOREIGN KEY ([ClientId]) REFERENCES [Client]([Id])
--- CoCowork.DB/Tables/RoomOrder.sql
+++ CoCowork.DB/Tables/RoomOrder.sql
@@ -5,7 +5,7 @@
     [OrderId] INT NOT NULL, 
     [StartDate] DATETIME NOT NULL, 
     [EndDate] DATETIME NOT NULL, 
-    [SubtotalPrice] DECIMAL NULL, 
+    [SubtotalPrice] DECIMAL(10, 2) NOT NULL, 
     CONSTRAINT [FK_RoomId_to_Room] FOREIGN KEY ([RoomId]) REFERENCES [Room]([Id]), 
     CONSTRAINT [FK_OrderId_to_Order] FOREIGN KEY ([OrderId]) REFERENCES [Order]([Id])
 )
