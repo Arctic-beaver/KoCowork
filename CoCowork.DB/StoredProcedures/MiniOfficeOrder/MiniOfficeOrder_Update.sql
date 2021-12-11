7a48956bfc74c5f4eb86b5a3dbd99d58e2a9cb49
Arctic Beaver <ArcticBeaver@yandex.ru>
2021-12-11 13:08:29 +0300

    Deleted updating OrderId
    From MiniOfficeOrder and PlaceOrder

--- CoCowork.DB/StoredProcedures/MiniOfficeOrder/MiniOfficeOrder_Update.sql
+++ CoCowork.DB/StoredProcedures/MiniOfficeOrder/MiniOfficeOrder_Update.sql
@@ -1,7 +1,6 @@
 ﻿CREATE PROC dbo.MiniOffficeOrder_Update
 	@Id int,
 	@MiniOfficeId int,
-	@OrderId int,
 	@StartDate datetime,
 	@EndDate datetime,
 	@SubtotalPrice decimal(10,2)
@@ -9,7 +8,6 @@ AS
 BEGIN
 	update dbo.MiniOfficeOrder
 	set MiniOfficeId = @MiniOfficeId,
-		OrderId = @OrderId,
 		StartDate = @StartDate,
 		EndDate = @EndDate,
 		SubtotalPrice = @SubtotalPrice
--- CoCowork.DB/StoredProcedures/PlaceOrder/PlaceOrder_Update.sql
+++ CoCowork.DB/StoredProcedures/PlaceOrder/PlaceOrder_Update.sql
@@ -1,7 +1,6 @@
 ﻿CREATE PROC dbo.PlaceOrder_Update
 	@Id int,
 	@PlaceId int,
-	@OrderId int,
 	@StartDate datetime,
 	@EndDate datetime,
 	@SubtotalPrice decimal(10,2)
@@ -9,7 +8,6 @@ AS
 BEGIN
 	update dbo.PlaceOrder
 	set PlaceId = @PlaceId,
-		OrderId = @OrderId,
 		StartDate = @StartDate,
 		EndDate = @EndDate,
 		SubtotalPrice = @SubtotalPrice
