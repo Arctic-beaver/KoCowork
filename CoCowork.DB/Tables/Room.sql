161fd82a714431ec485deab7743ee104d6edc684
Arctic Beaver <ArcticBeaver@yandex.ru>
2021-12-11 14:07:52 +0300

    Fixed bug in Room
    Deleted second primery key

--- CoCowork.DB/Tables/Room.sql
+++ CoCowork.DB/Tables/Room.sql
@@ -4,6 +4,5 @@
     [TypeId] INT NOT NULL, 
     [AmountOfPeople] INT NOT NULL, 
     [PricePerHour] DECIMAL(10, 2) NOT NULL, 
-    PRIMARY KEY ([Id]), 
     CONSTRAINT [FK_TypeId_to_RoomType] FOREIGN KEY (TypeId) REFERENCES [RoomType]([Id])
 )
