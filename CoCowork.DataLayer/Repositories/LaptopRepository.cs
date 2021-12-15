094828c0a353d08e2d9d0914c7eda31c352a5bf0
BarBoss228 <nastia.polpoudenko@gmail.com>
2021-12-15 13:47:39 +0300

    Dapper CRUD methods for LaptopRepository were added

--- CoCowork.DataLayer/Repositories/LaptopRepository.cs
+++ CoCowork.DataLayer/Repositories/LaptopRepository.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CoCowork.DataLayer.Entities;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,68 @@ namespace CoCowork.DataLayer.Repositories
     public class LaptopRepository
     {
         private const string _connString = "Server=(local);Integrated Security=True;Database=CoCowork.DB;";
+        private const string _selectAllProc = "dbo.Laptop_SelectAll";
+        private const string _selectByIdProc = "dbo.Laptop_SelectById";
+        private const string _insertProc = "dbo.Laptop_Insert";
+        private const string _updateProc = "dbo.Laptop_Update";
+        private const string _deleteProc = "dbo.Laptop_Delete";
+
+        public List<Laptop> GetAllLaptops()
+        {
+            using var connection = new SqlConnection(_connString);
+            connection.Open();
+            var result = connection.Query<Laptop>(_selectAllProc).ToList();
+            return result;
+        }
+        public Laptop GetLaptopsById(int id)
+        {
+            using var connection = new SqlConnection(_connString);
+            connection.Open();
+
+            return connection
+                .QueryFirstOrDefault<Laptop>(
+                _selectByIdProc,
+                new { Id = id },
+                commandType: CommandType.StoredProcedure);
+        }
+        public void Add(Laptop laptop)
+        {
+            using var connection = new SqlConnection(_connString);
+            connection.Open();
+
+            connection.Execute(_insertProc,
+                new
+                {
+                   Name = laptop.Name,
+                   Amount = laptop.Amount,
+                   Price = laptop.Price,
+                   Description = laptop.Description
+                },
+                commandType: CommandType.StoredProcedure);
+        }
+        public void UpdateLaptopById(int id, Laptop laptop)
+        {
+            using var connection = new SqlConnection(_connString);
+            connection.Open();
+
+            connection.Execute(_updateProc,
+                new
+                {
+                    Id = id,
+                    Name = laptop.Name,
+                    Amount = laptop.Amount,
+                    Price = laptop.Price,
+                    Description = laptop.Description
+                },
+                commandType: CommandType.StoredProcedure);
+        }
+        public void DeleteLaptopById(int id)
+        {
+            using var connection = new SqlConnection(_connString);
+            connection.Open();
+            connection.Execute(_deleteProc,
+                        new { Id = id },
+                        commandType: CommandType.StoredProcedure);
+        }
     }
 }
