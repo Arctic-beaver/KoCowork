3fbb1706c2c9091788befd24413cb8f141893235
Arctic Beaver <ArcticBeaver@yandex.ru>
2021-12-17 00:16:13 +0300

    MainWindowScratch is ready + helpers for grids

---
+++ CoCoworkView/UIHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CoCowork.UI
+{
+    public static class UIHelpers
+    {
+        public static void SetButtonEnabledAndVisibility(Grid grid, bool active)
+        {
+            if (active)
+            {
+                grid.Visibility = Visibility.Visible;
+                grid.IsEnabled = true;
+            }
+            else
+            {
+                grid.Visibility = Visibility.Hidden;
+                grid.IsEnabled = false;
+            }
+        }
+    }
+}
