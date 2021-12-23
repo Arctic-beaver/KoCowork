8cba372fb9bfd12b3bd4a42768689f933c7c94a0
BarBoss228 <nastia.polpoudenko@gmail.com>
2021-12-24 01:29:18 +0300

    Created a raw UI for Product
    Also added Product Model

---
+++ CoCowork.BusinessLayer/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoCowork.BusinessLayer.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
--- CoCoworkView/ViewModels/ProductViewModel.cs
+++ CoCoworkView/ViewModels/ProductViewModel.cs
@@ -1,12 +1,73 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace CoCowork.UI.ViewModels
 {
     public class ProductViewModel : TabViewModel
     {
+        
+        private string _name;
+        private string _description;
+        private int _amount;
+        private decimal _priceForOne;
+
+        public ObservableCollection<ProductViewModel> Products { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public int Amount
+        {
+            get => _amount;
+            set
+            {
+                _amount = value;
+                OnPropertyChanged(nameof(Amount));
+            }
+        }
+
+        public decimal PriceForOne
+        {
+            get => _priceForOne;
+            set
+            {
+                _priceForOne = value;
+                OnPropertyChanged(nameof(PriceForOne));
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        public ProductViewModel(ProductModel dataProduct)
+        {
+            Products = new ObservableCollection<ProductViewModel>();
+            Name = dataProduct.Name;
+            Description = dataProduct.Description;
+            Amount = dataProduct.Amount;
+            PriceForOne = dataProduct.PriceForOne;
+        }
+
+
+
         public ProductViewModel()
         {
             GridVisibility = Visibility.Hidden;
         }
     }
 }
+
