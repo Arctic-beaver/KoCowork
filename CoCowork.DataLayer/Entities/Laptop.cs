a1cfceabdbd0f5790a5795019a0368199165579c
Алексей Коровин <bennykorovin@gmail.com>
2021-12-26 17:20:50 +0300

    Laptop entity fixed

--- CoCowork.DataLayer/Entities/Laptop.cs
+++ CoCowork.DataLayer/Entities/Laptop.cs
@@ -7,7 +7,7 @@ namespace CoCowork.DataLayer.Entities
         public int Id { get; set; } 
         public string Name { get; set; }    
         public int Amount { get; set; }
-        public decimal Price { get; set; }
+        public decimal PricePerMonth { get; set; }
         public string Description { get; set; }
     }
 }
