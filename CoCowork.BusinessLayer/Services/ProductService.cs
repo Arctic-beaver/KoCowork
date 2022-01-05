0b4caeba0bdb976fac10aac3270e521eca56806a
BarBoss228 <nastia.polpoudenko@gmail.com>
2022-01-05 16:36:46 +0300

    Added usings

--- CoCowork.BusinessLayer/Services/ProductService.cs
+++ CoCowork.BusinessLayer/Services/ProductService.cs
@@ -1,7 +1,7 @@
-﻿using CoCowork.BusinessLayer.Models;
+﻿using CoCowork.BusinessLayer.Configuration;
+using CoCowork.BusinessLayer.Models;
 using CoCowork.DataLayer.Entities;
 using CoCowork.DataLayer.Repositories;
-using PseudoCalc.BusinessLayer.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
