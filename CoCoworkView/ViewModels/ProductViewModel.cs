8c17c9f8a248875b3bf3dc888ffe35f73019b32a
BarBoss2228 <nastia.polpoudenko@gmail.com>
2022-01-07 21:10:24 +0300

    Still not working

--- CoCoworkView/ViewModels/ProductViewModel.cs
+++ CoCoworkView/ViewModels/ProductViewModel.cs
@@ -10,10 +10,7 @@ namespace CoCowork.UI.ViewModels
     public class ProductViewModel : InnerGridViewModel
     {
         
-        private string _name;
-        private string _description;
-        private int _amount;
-        private decimal _priceForOne;
+        
         private bool _productsInStock;
         private ProductModel _selectedItem;
         
@@ -36,45 +33,7 @@ namespace CoCowork.UI.ViewModels
 
         public ObservableCollection<ProductModel> Products { get; set; }
 
-        public string Name
-        {
-            get => _name;
-            set
-            {
-                _name = value;
-                OnPropertyChanged(nameof(Name));
-            }
-        }
-
-        public int Amount
-        {
-            get => _amount;
-            set
-            {
-                _amount = value;
-                OnPropertyChanged(nameof(Amount));
-            }
-        }
-
-        public decimal PriceForOne
-        {
-            get => _priceForOne;
-            set
-            {
-                _priceForOne = value;
-                OnPropertyChanged(nameof(PriceForOne));
-            }
-        }
-
-        public string Description
-        {
-            get => _description;
-            set
-            {
-                _description = value;
-                OnPropertyChanged(nameof(Description));
-            }
-        }
+       
 
         public bool ProductsInStock
         {
