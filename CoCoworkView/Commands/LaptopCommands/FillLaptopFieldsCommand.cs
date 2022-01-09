31318546fd0773087552ae2f0860b0451316eb03
Lekksha <bennykorovin@gmail.com>
2022-01-09 17:21:07 +0300

    FillLaptopFieldsCommand was added

---
+++ CoCoworkView/Commands/LaptopCommands/FillLaptopFieldsCommand.cs
@@ -0,0 +1,21 @@
+﻿using CoCowork.UI.ViewModels;
+
+namespace CoCowork.UI.Commands.MiniOfficeCommands
+{
+    public class FillLaptopFieldsCommand : CommandBase
+    {
+        private readonly LaptopViewModel _vm;
+
+        public FillLaptopFieldsCommand(LaptopViewModel vm)
+        {
+            _vm = vm;
+        }
+        public override void Execute(object parameter)
+        {
+            _vm.Name = _vm.SelectedLaptop.Name;
+            _vm.Number = _vm.SelectedLaptop.Number;
+            _vm.PricePerMonth = _vm.SelectedLaptop.PricePerMonth;
+            _vm.Description = _vm.SelectedLaptop.Description;
+        }
+    }
+}
