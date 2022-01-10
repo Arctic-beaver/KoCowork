13182f49d9702c55621bc177b8a07cb01d437409
BarBoss228 <nastia.polpoudenko@gmail.com>
2022-01-10 23:01:32 +0300

    Trying to get rid of extra column

--- CoCowork.BusinessLayer/Models/ClientModel.cs
+++ CoCowork.BusinessLayer/Models/ClientModel.cs
@@ -4,8 +4,6 @@ namespace CoCowork.BusinessLayer.Models
 {
     public class ClientModel : ClientShortModel
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public DateTime? DateBirth { get; set; }
